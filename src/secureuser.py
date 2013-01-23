b49482722a46e4436c080f7c917278c7bd64cebe
Thomas Spycher <me@tspycher.com>
2013-01-23 14:56:58 +0100

    Some minor enhancements
    - removed key caching, addes Session keys
    - added tests for wrong password tests
    - completed function for security level 2 password restores
    - added documentation

--- src/secureuser.py
+++ src/secureuser.py
@@ -5,26 +5,49 @@ import random
 import time
 import pickle
 
+class WrongPasswordError(Exception):
+    '''
+    Exception Class to handel wrong passwords
+    '''
+    
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 class SecureUser(object):
-    public_key = None
-    _private_key = None      # symetrical encrypted private key.
-    _private_key_rescue = None    # symetrical encrypted private key. For rescueing if the primary gets lost.
+    '''
+    The SecureUser Class provides strong encryption support for userdata. It also provides the controll of the security level.
+    Increasing the security level, decreases the ability to restore a forgotten password. Level 3 does not provide any possiblity
+    of restoring a lost password. If this happens all userdata is lost.
     
-    _encrypted_symetrical_key = None               # used to encrypt all data. the key itself is encrypted with the asymetrical key
+    1. A new RSA Keypair for the user gets created.
+    2. The Private Key gets symmetrical encrypted with password of the user. In security Level 1&2 the key gets additional encrypted as a rescue element
+    3. A new 32Byte long symmetrical key gets created. This key gets used to encrypt all userdata
+    4. The masterkey gets encrypted with the private key of the user 
+    '''
+    public_key = None                       #: The public key of this object
+    security_level = 1                      #: security Level of the user: 1 = insecure, 2 = more secure, 3 = highes security
     
-    _sym_blocksize = 32
-    _sym_padding = '{'
+    _private_key = None                     #: symetrical encrypted private key.
+    _private_key_rescue = None              #: symetrical encrypted private key. For rescueing if the primary gets lost.
+    _encrypted_symetrical_key = None        #: used to encrypt all data. the key itself is encrypted with the asymetrical key
     
-    _symkey_cache_enabled = True
-    _symkey_cache = None
-    _symkey_cache_time = 0
-    _symkey_cache_timeout = 2
+    _sym_blocksize = 32                     #: Symmetrical Blocksize
+    _sym_padding = '{'                      #: Padding character for smaller blocksizes
+        
+    _session_key = None                     #: Symmetrical encrypted session key
+    _session_key_time = None                #: The timestamp of the time the key has last used
+    _session_key_timeout = 2                #: Timout in seconds after the session_key gets purged
     
-    def __init__(self, userpassword):
-        self._create_keymaterial(userpassword)
+    def __init__(self, userpassword, level = 1, admin_public_key = None):
+        self._create_keymaterial(userpassword, level, admin_public_key)
     
-    def _create_keymaterial(self, userpassword):
+    def _create_keymaterial(self, userpassword, level = 1, admin_public_key = None):
+        '''
+        Initializes all the keymaterial for a new object
+        '''
+        self.security_level = level
         # Create 2048bit strong keypair and load them into a RSA object
         keys = RSA.gen_key(bits=2048, e=7)
         keyPair = BIO.MemoryBuffer()
@@ -37,56 +60,83 @@ class SecureUser(object):
         rsa.save_key_bio(pri_mem, None)
         rsa.save_pub_key_bio(pub_mem)
         self.public_key = pub_mem.getvalue()
-
-        # Get the private key and encrypt it 
-        self._private_key = self._sym_encrypt(pri_mem.getvalue(), userpassword)
-        #self._private_key_rescue = self._sym_encrypt(pri_mem.getvalue(), "adminpw")
+        
+        # Get the private key and encrypt it
+        # The rescue key gets only created of a userlevel below three has been choosen
+        p = pri_mem.getvalue()
+        self._private_key = self._sym_encrypt(p, userpassword)
+        if self.security_level < 3:
+            if self.security_level <= 1:
+                # symmetrical encryption of the rescue key
+                self._private_key_rescue = self._sym_encrypt(p, "adminpw")
+            else:
+                # asymmetrical encryption of the recue key with the keypair of the admin
+                x = RSA.load_pub_key_bio(BIO.MemoryBuffer(admin_public_key))
+                chunksize = 245 #246 byte are too much for rsa encryption
+                y = []
+                for i in range((len(p)/chunksize)+1):
+                    y.append(base64.b64encode( x.public_encrypt((p[i * chunksize:(i * chunksize)+chunksize]),1) ) )
+                self._private_key_rescue = "\n".join(y)
         
         # Create the Symetricalkey for the encryption of user data. Key gets encrypted with the private key of the user
         self._encrypted_symetrical_key = self._async_encrypt(self._random_key(), userpassword)
     
-    def encrypt(self, data, userpassword):
+    def unlock(self, userpassword):
+        '''
+        Unlocks the symmetrical master key with the password of the user and creates
+        a new random session password which gets used to symmetrical encrypt the the master
+        key for this session.
+        '''
+        session_password = self._random_key()
+        self._session_key = self._sym_encrypt(self._sym_key(userpassword), session_password)
+        self._session_key_time = time.time()
+        return session_password
+    
+    def raw_keys(self, userpassword):
+        '''
+        Returns the raw key material
+        '''
+        try:
+            return {"public_key":self.public_key,
+                    "private_key":self._sym_decrypt(self._private_key, userpassword),
+                    "symmetrical_key":self._sym_key(userpassword)}
+        except:
+            raise WrongPasswordError("Could not get keymaterial")
+    
+    def encrypt(self, data, session_password):
         '''
         Symmetrical encryption of userdata. Data can be as big as possible.
         '''
-        key = self._sym_key(userpassword)
+        key = self._key(session_password)
         return self._sym_encrypt(data, key)
     
-    def decrypt(self, data, userpassword):
+    def decrypt(self, data, session_password):
         '''
         Symmetrical Decryption of userdata
         '''
-        key = self._sym_key(userpassword)
+        key = self._key(session_password)
         return self._sym_decrypt(data, key)
-
+    
+    def _key(self, session_password):
+        '''
+        Returns the Symmetrical key temporary encrypted with the session secret
+        '''
+        if (time.time() - self._session_key_time) >= self._session_key_timeout:
+            return None
+        self._session_key_time = time.time()
+        return self._sym_decrypt(self._session_key, session_password)
+    
     def _sym_key(self, userpassword):
         '''
         Returns the Symmetrical key of the user
         '''
-        
-        if self._symkey_cache and self._symkey_cache_enabled:
-            if (time.time() - self._symkey_cache_time) >= self._symkey_cache_timeout:
-                self._symkey_cache = None
-                self._symkey_cache_time = None
-                print "Resetting key"
-            else:
-                self._symkey_cache_time = time.time()
-                return self._symkey_cache
-            
-        self._symkey_cache_time = time.time()
-        self._symkey_cache = self._async_decrypt(self._encrypted_symetrical_key, userpassword)
-        if not self._symkey_cache_enabled:
-            x = self._symkey_cache
-            self._symkey_cache = None
-            return x
-        return self._symkey_cache
+        return self._async_decrypt(self._encrypted_symetrical_key, userpassword)
     
     def _sym_encrypt(self, data, secret):
         '''
         Final symmetrical encryption of given data with AES256
         '''
-        secret = secret + (self._sym_blocksize - len(secret) % self._sym_blocksize) * self._sym_padding
-        cipher = AES.new(secret)
+        cipher = self._cipher(secret)
         data = data + (self._sym_blocksize - len(data) % self._sym_blocksize) * self._sym_padding
         return base64.b64encode(cipher.encrypt(data))
     
@@ -94,10 +144,19 @@ class SecureUser(object):
         '''
         Final symmetrical decryption of given data with AES256
         '''
-        secret = secret + (self._sym_blocksize - len(secret) % self._sym_blocksize) * self._sym_padding
-        cipher = AES.new(secret)
+        cipher = self._cipher(secret)
         return cipher.decrypt(base64.b64decode(data)).rstrip(self._sym_padding)
     
+    def _cipher(self,secret):
+        '''
+        Gets an AES based cipher object
+        '''
+        try:
+            secret = secret + (self._sym_blocksize - len(secret) % self._sym_blocksize) * self._sym_padding
+            return AES.new(secret)
+        except:
+            raise WrongPasswordError("Could not create cipher")
+        
     def _random_key(self, lenght = 32):
         '''
         Generates an random key of the given lenght
@@ -106,6 +165,7 @@ class SecureUser(object):
         pw = []
         for i in range(lenght-1):
             pw.append(chars[random.randint(0,len(chars)-1)])
+            i
         return "".join(pw)
     
     ## Asymetric
@@ -113,8 +173,11 @@ class SecureUser(object):
         '''
         Gets the RSA Object
         '''
-        return RSA.load_key_string("%s%s" % (self.public_key, self._sym_decrypt(self._private_key, userpassword)))
-    
+        try:
+            return RSA.load_key_string("%s%s" % (self.public_key, self._sym_decrypt(self._private_key, userpassword)))
+        except:
+            raise WrongPasswordError("Could not create RSA Object")
+            
     def _async_encrypt(self, data, userpassword):
         '''
         Asymmetrical RSA Encryption of given data
@@ -133,9 +196,45 @@ class SecureUser(object):
             self.logger.log.error("Could not decrypt")
             return str()
 
-    def restore_user_password(self):
-        pass
+    def restore_user_password(self, newpassword, adminpassword = None, admin_private_key = None):
+        '''
+        Public method to recover a lost password.
+        '''
+        if self.security_level == 1:
+            return self._restore_level1(newpassword, adminpassword)
+        
+        if self.security_level == 2 and admin_private_key:
+            return self._restore_level2(newpassword, admin_private_key)
     
+    def _restore_level1(self, newpassword, adminpassword):
+        '''
+        Restores a lost passwort of an user with security level 1
+        The Rescue password is symmetrical encrypted
+        '''
+        x = self._sym_decrypt(self._private_key_rescue, adminpassword)
+        try:
+            RSA.load_key_string(x)
+        except:
+            raise WrongPasswordError("Could not restore Private Key with Admin Password")
+        self._private_key = self._sym_encrypt(x, newpassword)
+        return True
+
+    def _restore_level2(self, newpassword, admin_private_key):
+        '''
+        Restores a lost password of an user with security level 2
+        The Rescue password is asymmetrical encrypted with an admin key pair where the private key is external
+        '''
+        #print self._private_key_rescue
+        rsa = RSA.load_key_string(admin_private_key)
+        privateKey = []
+        for x in self._private_key_rescue.splitlines():
+            privateKey.append(rsa.private_decrypt(base64.b64decode(x), 1))
+        #x = rsa.private_decrypt(base64.b64decode(self._private_key_rescue), 1)
+        # print "".join(privateKey)
+        #x = self._async_decrypt(self._private_key_rescue, admin_private_key)
+        self._private_key = self._sym_encrypt("".join(privateKey), newpassword)
+        return True
+
     def __getstate__(self):
         '''
         Makes sure that only allowed attributes getting serialized
--- test/encryptiontest.py
+++ test/encryptiontest.py
@@ -6,32 +6,87 @@ Created on Dec 19, 2012
 import unittest
 import time
 
-from secureuser import SecureUser
+from secureuser import SecureUser, WrongPasswordError
 
 class EncryptionTest(unittest.TestCase):
     def setUp(self):
         pass
-
+    
     def tearDown(self):
         pass
-
+    
+    def test_password_recovery_level1(self):
+        password = "Pas$word"
+        su = SecureUser(password)
+        sessionKey = su.unlock(password)
+        text = "This is secure"
+        encrypted = su.encrypt(text, sessionKey)
+        self.assertEqual(text, su.decrypt(encrypted, sessionKey), "Could not en/decrypt userdata")
+        
+        print "Restoring password"
+        adminpassword = "adminpw"
+        newPassword = "Pa$sword2"
+        su.restore_user_password(newPassword, adminpassword)
+        sessionKey = su.unlock(newPassword)
+        self.assertEqual(text, su.decrypt(su.encrypt(text, sessionKey), sessionKey), "Could not en/decrypt userdata after password reset")
+        self.assertEqual(text, su.decrypt(encrypted, sessionKey), "Could not en/decrypt userdata encrypted with privious key")
+    
+    def test_wrong_passwords(self):
+        password = "Pas$word"
+        su = SecureUser(password)
+        text = "This text is secure"
+        self.assertRaises(WrongPasswordError, su.raw_keys, "ThisPwi$Wrong")
+        self.assertRaises(WrongPasswordError, su.unlock, "thisPasswordIsWrong")
+        sessionKey = su.unlock(password)
+        self.assertRaises(WrongPasswordError, su.encrypt, text, "1234567891234567")
+        encrypted = su.encrypt(text, sessionKey)
+        self.assertRaises(WrongPasswordError, su.decrypt, encrypted, "1234567891234567")
+        decrypted = su.decrypt(encrypted, sessionKey)
+        self.assertEqual(decrypted, text, "Decryption is not equal")
+        self.assertRaises(WrongPasswordError, su.restore_user_password, "ThisPwI$new", "WrongAdminPassword")
+        
+    def test_password_recovery_level2(self):
+        # Create an Admin User and get his keys
+        admin = SecureUser("adminpw")
+        keys = admin.raw_keys("adminpw")
+        
+        password = "Pas$word"
+        su = SecureUser(password, 2, admin_public_key = keys['public_key'])
+        sessionKey = su.unlock(password)
+        text = "This is secure"
+        encrypted = su.encrypt(text, sessionKey)
+        self.assertEqual(text, su.decrypt(encrypted, sessionKey), "Could not en/decrypt userdata")
+        
+        print "Restoring password"
+        adminpassword = "adminpw"
+        newPassword = "Pa$sword2"
+        su.restore_user_password(newPassword, admin_private_key = keys['private_key'])
+        
+        sessionKey = su.unlock(newPassword)
+        self.assertEqual(text, su.decrypt(su.encrypt(text, sessionKey), sessionKey), "Could not en/decrypt userdata after password reset")
+        self.assertEqual(text, su.decrypt(encrypted, sessionKey), "Could not en/decrypt userdata encrypted with privious key")
+        
     def test_serialisation(self):
         password = "Pas$word"
         text = "Ich bin ein Test"
         
         su = SecureUser(password)
+        sessionKey = su.unlock(password)
         su_serial = su.serialize()
         
         self.assertTrue(su_serial, "No serialized object gotten")
-        self.assertEqual(text, su.decrypt(su.encrypt(text, password), password), "Could not en/decrypt userdata")
+        self.assertEqual(text, su.decrypt(su.encrypt(text, sessionKey), sessionKey), "Could not en/decrypt userdata")
         su = None
-        
+        print "Serialisation done"
         suNew = SecureUser.unserialize(su_serial)
-        self.assertEqual(text, suNew.decrypt(suNew.encrypt(text, password), password), "Could not en/decrypt userdata afer serialisation")
+        sessionKeyNew = suNew.unlock(password)
+        self.assertEqual(text, suNew.decrypt(suNew.encrypt(text, sessionKeyNew), sessionKeyNew), "Could not en/decrypt userdata afer serialisation")
+        print "Deserialisation done"
         
-    def test_createProduct(self):
+    def test_encryption_performance(self):
         password = "Pas$word"
         su = SecureUser(password)
+        sessionKey = su.unlock(password)
         self.assertTrue(su.public_key, "There is no publickey")
         self.assertTrue(su._private_key, "no private key")
         
@@ -48,12 +103,12 @@ class EncryptionTest(unittest.TestCase):
         for i in range(numMessages):
             data = "%i -%s" % (i, text)
             origMessages.append(data)
-            messages.append(su.encrypt(data, password))
+            messages.append(su.encrypt(data, sessionKey))
         timeEncryption = time.time()
         
         print "Encryption took %f seconds" % (timeEncryption-startTime)
         for i in range(numMessages):
-            messages[i] = su.decrypt(messages[i], password)
+            messages[i] = su.decrypt(messages[i], sessionKey)
             self.assertEqual(messages[i], origMessages[i], "The message has changed during decryptiong")
         timeDecryption = time.time()
 
