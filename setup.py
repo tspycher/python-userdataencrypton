10f1cf33b01dc4ceb764b68c0aa061c9ea163a63
Thomas Spycher <me@tspycher.com>
2013-02-01 00:20:07 -0800

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Userdataencryption',
+      version='0.1',
+      description='A user model supporting encryption of its data',
+      author='Thomas Spycher',
+      author_email='me@tspycher.com',
+      url='https://github.com/tspycher/python-userdataencrypton.git',
+      packages=['secureuser'],
+     )
